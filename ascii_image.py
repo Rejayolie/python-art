5be3ecb78be6edc81998eb8e950b11fe4af02cb3
ymujahid <yunusmujahid5@gmail.com>
2020-05-05 11:10:18 +0100

    added ascii_image.py to master

---
+++ ascii_image.py
@@ -0,0 +1,59 @@
+from PIL import Image
+import numpy as np
+import pprint
+
+ASCII_CHARS = ['#', '?', ' ', '.', '=', '+', '.', '*', '3', '&', '@']
+fp = 'zeroToMasteryIcon.png'
+
+
+# open image
+def open_image(file_path):
+    image = Image.open(file_path)
+    return image
+
+
+# resize the image
+def scale_image(image, new_width=50):
+    original_height, original_width = image.size
+    aspect_ratio = int(original_height/original_width)
+    new_height = aspect_ratio*new_width
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+
+# convert to grayscale
+def convert_to_greyscale(image):
+    grey_image = image.convert('L')
+    grey_image.save('new_file.png')
+    return grey_image
+
+
+# convert pixels to ascii characters
+def map_pixels_to_ascii(image, range_width=25):
+    """
+    maps each pixel into ascii characters based on the range it falls
+    range_width: divides the ultimate intensity range of the pixel value, 0 - 255, into 25 sub-ranges
+    range_width is used mainly to prevent the pixels_to_char comprehension from throwing error
+    and ensure that pixels with the same pixel values have the same ascii values
+    """
+    pixels_to_char = []
+    pixels_in_image = list(image.getdata())
+    pixels_to_char = [ASCII_CHARS[int(pixel_value / range_width)] for pixel_value in pixels_in_image]
+    pixel_char = "".join(pixels_to_char)
+    return pixel_char
+
+
+# convert the whole image to ascii
+def image_to_ascii(image):
+    new_width = 50
+    image = scale_image(image)
+    image = convert_to_greyscale(image)
+    pixels_to_ascii_chars = map_pixels_to_ascii(image)
+    len_pixels_to_ascii_chars = len(pixels_to_ascii_chars)
+    image_ascii = [pixels_to_ascii_chars[index: index + new_width] for index in range(0, len_pixels_to_ascii_chars, new_width)]
+    return "\n".join(image_ascii)
+
+
+print(image_to_ascii(open_image(fp)))
+
+
